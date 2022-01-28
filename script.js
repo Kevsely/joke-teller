bfc125e3139cc301c6f336e09c7214d590d11f64
Kevsely <elegis.sossou@yahoo.com>
2022-01-28 23:34:13 +0800

    Binding the button to the joke API fecthing

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const jokeApiUrl = 'https://v2.jokeapi.dev/joke/Programming'
+// DOM Elements
+const button = document.getElementById('button');
+
+const jokeApiUrl = 'https://v2.jokeapi.dev/joke/Programming';
+
+// Toggle button while fetching and listening joke
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
 
 // Get the jokes from API
 async function getJoke() {
     try {
+        toggleButton();
+
         const response = await fetch(jokeApiUrl);
         const data = await response.json();
 
-        console.log(data)
+        console.log(data);
+
+        toggleButton();
     } catch (error) {
         console.log('Something went wrong', error);
     }
 }
 
-getJoke();
+// Event listeners
+button.addEventListener('click', getJoke);
