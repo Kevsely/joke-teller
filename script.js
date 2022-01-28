dfb116a5d1a7b03163bfc38e48ba00523765e9f4
Kevsely <elegis.sossou@yahoo.com>
2022-01-28 23:44:50 +0800

    Some formatting for single or two part jokes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const button = document.getElementById('button');
 
 const jokeApiUrl = 'https://v2.jokeapi.dev/joke/Programming';
+let jokeText;
 
 // Toggle button while fetching and listening joke
 function toggleButton() {
@@ -16,7 +17,14 @@ async function getJoke() {
         const response = await fetch(jokeApiUrl);
         const data = await response.json();
 
+        // For single part joke
+        if(data.type === 'single')
+            jokeText = data.joke;
+        else
+            jokeText = `${data.setup}... ${data.delivery}`;
+        
         console.log(data);
+        console.log(jokeText);
 
         toggleButton();
     } catch (error) {
