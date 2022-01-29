d52feae5409e8779324c5b71838e6d2ea0c2018f
Kevsely <elegis.sossou@yahoo.com>
2022-01-29 11:48:02 +0100

    Adding tts script to the project Removing audio element from the project

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// DOM Elements
-const button = document.getElementById('button');
-
 const jokeApiUrl = 'https://v2.jokeapi.dev/joke/Programming';
 let jokeText;
+const ttsApiKey = '6c6845a1f1484c2fa00bcbba44a4984b';
+
+// DOM Elements
+const button = document.getElementById('button');
 
 // Toggle button while fetching and listening joke
 function toggleButton() {
@@ -17,20 +18,29 @@ async function getJoke() {
         const response = await fetch(jokeApiUrl);
         const data = await response.json();
 
-        // For single part joke
-        if(data.type === 'single')
+        
+        if(data.type === 'single') // For single part joke
             jokeText = data.joke;
         else
-            jokeText = `${data.setup}... ${data.delivery}`;
-        
-        console.log(data);
-        console.log(jokeText);
+            jokeText = `${data.setup}... ${data.delivery}`; // for two parts joke
 
         toggleButton();
     } catch (error) {
         console.log('Something went wrong', error);
     }
 }
+
+// Test function to test Text-to-Speech API
+VoiceRSS.speech({
+    key: ttsApiKey,
+    src: 'Hello, world!',
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0, 
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false
+});
 
 // Event listeners
 button.addEventListener('click', getJoke);
